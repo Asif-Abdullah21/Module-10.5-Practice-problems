2d1272d26f4f89a0e91ed696ed4446242ce57022
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-18 00:21:28 +0600

    I. Palindrome

--- I_Palindrome.c
+++ I_Palindrome.c
@@ -93,3 +93,50 @@ int main()
 
 
 */
+
+/* credit: muhammad_sayem
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[1000];
+    scanf("%s", str1);
+    char str2[1000];
+    int i;
+    int len = strlen(str1);
+    char temp;
+
+    // Copying the string to another string //
+
+    for(i=0; i<len; i++){
+        str2[i] = str1[i];
+    }
+    str2[len] = '\0';           // If we don't fix the last index of the second string with a null char, then it can give garbage value or wrong answer to the code //
+
+    // Reversing the original string //
+
+    for(i=0; i<len/2; i++){
+        temp = str1[i];
+        str1[i] = str1[len-1-i];
+        str1[len-1-i] = temp;
+    }
+
+    // Comparing main string and the copied string //
+
+    int flag = 0;
+
+    for(i=0; str1[i] != '\0' || str2[i] != '\0'; i++){
+        if(str1[i] != str2[i]){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0) printf("YES\n");
+    else printf("NO\n");
+
+    return 0;
+}
+
+*/
